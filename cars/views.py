fb00c830a2fc1a9a429ebb8f55fec1c3c90739c3
sumancfc <sumanstha999@gmail.com>
2021-01-10 16:56:55 +0545

    Cars model, featured cars, latest cars added

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Car
+
 
 # Create your views here.
+def cars(request):
+       all_cars = Car.objects.order_by('-created_date')
+
+       data = {
+             'all_cars' : all_cars
+       }
+
+       return render(request, 'cars/cars.html', data)
+
+def car_detail(request):
+       return render(request, 'cars/car_detail.html')
+
+def search(request):
+       return render(request, 'cars/search.html')
